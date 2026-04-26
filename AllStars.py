ee9e0997a849292cad14b360a171032bd10a32cf
unknown <lucastaufer001@gmail.com>
2026-04-26 00:44:16 -0300

    ta tudo errrado

--- AllStars.py
+++ AllStars.py
@@ -1,3 +1,24 @@
 import subprocess
+import time
+import os
+import cv2
+import numpy as np
+import mss
+import keyboard
+import pyautogui
 
+time.sleep(3)
+
+subprocess.run(["py", "./Funções/entrar_warp.py"])
+time.sleep(1)
+subprocess.run(["py", "./Funções/abrir_clicar_orbs.py"])
+time.sleep(1)
+subprocess.run(["py", "./Funções/entrar_warp.py"])
+time.sleep(1)
 subprocess.run(["py", "./Funções/abrir_itens.py"])
+time.sleep(1)
+subprocess.run(["py", "./Funções/procurar_portais.py"])
+time.sleep(1)
+subprocess.run(["py", "./Funções/fraqueza.py"])
+time.sleep(1)
+subprocess.run(["py", "./Funções/setar_fraqueza.py"])
---
+++ Funções/entrar_warp.py
@@ -0,0 +1,60 @@
+import cv2
+import numpy as np
+import mss
+import os
+import time
+import keyboard
+import pyautogui
+
+base_dir = os.path.dirname(__file__)
+caminho = os.path.join(base_dir, "..", "Ibagens", "warp.png")
+
+template = cv2.imread(caminho, cv2.IMREAD_COLOR)
+template = cv2.cvtColor(template, cv2.COLOR_BGR2RGB)
+
+threshold = 0.7
+
+scales = np.linspace(0.7, 1.3, 10)
+
+with mss.mss() as sct:
+    monitor = sct.monitors[1]
+
+    while True:
+        img = np.array(sct.grab(monitor))
+        img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
+
+        best_val = 0
+        best_loc = None
+        best_size = None
+
+        for scale in scales:
+            resized = cv2.resize(template, None, fx=scale, fy=scale)
+
+            if resized.shape[0] > img.shape[0] or resized.shape[1] > img.shape[1]:
+                continue
+
+            res = cv2.matchTemplate(img, resized, cv2.TM_CCOEFF_NORMED)
+            _, max_val, _, max_loc = cv2.minMaxLoc(res)
+
+            if max_val > best_val:
+                best_val = max_val
+                best_loc = max_loc
+                best_size = resized.shape[:2]
+
+        if best_val >= threshold:
+            print("ACHOU!", best_val)
+
+            h, w = best_size
+
+            center_x = best_loc[0] + w // 2
+            center_y = best_loc[1] + h // 2
+
+            pyautogui.moveTo(center_x-10, center_y)
+            time.sleep(0.1)
+            pyautogui.moveTo(center_x, center_y)
+            time.sleep(0.1)
+
+
+            break
+        else:
+            print(f"Melhor match: {best_val:.2f}")
