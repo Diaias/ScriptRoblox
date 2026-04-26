b72c03c6b0259fd0670ba4ef2307808e77b23ca0
unknown <lucastaufer001@gmail.com>
2026-04-25 21:28:47 -0300

    fodace

--- Funções/abrir_itens.py
+++ Funções/abrir_itens.py
@@ -4,14 +4,15 @@ import mss
 import os
 import pyautogui
 
-# caminho correto
+# caminho
 base_dir = os.path.dirname(__file__)
-caminho = os.path.join(base_dir, "..", "Ibagens", "items.png")  # corrigido
+caminho = os.path.join(base_dir, "..", "Imagens", "items.png")
 
-# carregar template
 template = cv2.imread(caminho, cv2.IMREAD_COLOR)
 
 threshold = 0.8
+
+scales = np.linspace(0.5, 1.5, 20)
 
 with mss.mss() as sct:
     monitor = sct.monitors[1]
@@ -20,26 +21,34 @@ with mss.mss() as sct:
         img = np.array(sct.grab(monitor))
         img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
 
-        res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
-        _, max_val, _, max_loc = cv2.minMaxLoc(res)
+        best_val = 0
+        best_loc = None
+        best_size = None
 
-        if max_val >= threshold:
-            print("Imagem encontrada!", max_val)
+        for scale in scales:
+            resized = cv2.resize(template, None, fx=scale, fy=scale)
 
-            # pega tamanho do template
-            h, w = template.shape[:2]
+            if resized.shape[0] > img.shape[0] or resized.shape[1] > img.shape[1]:
+                continue
 
-            # calcula centro
-            center_x = max_loc[0] + w // 2
-            center_y = max_loc[1] + h // 2
+            res = cv2.matchTemplate(img, resized, cv2.TM_CCOEFF_NORMED)
+            _, max_val, _, max_loc = cv2.minMaxLoc(res)
 
-            print("Centro:", center_x, center_y)
+            if max_val > best_val:
+                best_val = max_val
+                best_loc = max_loc
+                best_size = resized.shape[:2]
 
-            # clica
+        if best_val >= threshold:
+            print("ACHOU!", best_val)
+
+            h, w = best_size
+
+            center_x = best_loc[0] + w // 2
+            center_y = best_loc[1] + h // 2
+
             pyautogui.click(center_x, center_y)
 
             break
         else:
             print("Não encontrou")
-
-cv2.destroyAllWindows()
